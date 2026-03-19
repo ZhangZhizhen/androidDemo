345b63a48a8aac9b53f2f34e1054a0b41a252467
zzz <zhizhen.zhang@rjoy.com>
2026-03-19 17:45:11 +0800

    feat: 接入 AI 自动分类衣服图片

--- app/src/main/java/org/Engine/Util/AppActivity.java
+++ app/src/main/java/org/Engine/Util/AppActivity.java
@@ -17,5 +17,6 @@ public class AppActivity extends AppCompatActivity {
         setContentView(R.layout.activity_app);
         Log.i("AppActivity", "onCreate");
         Log.i("AppActivity", "aaa");
+        Log.i("AppActivity", "bbb");
     }
 }
