718b2c56c5aa7c9f7bb263a4acecad30eb608c05
zzz <zhizhen.zhang@rjoy.com>
2026-03-19 15:07:48 +0800

    add while

--- app/src/main/java/org/Engine/Util/AppActivity.java
+++ app/src/main/java/org/Engine/Util/AppActivity.java
@@ -15,6 +15,8 @@ public class AppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
-        Log.i("AppActivity", "onCreate"+5/0);
+        while (true) {
+            Log.i("AppActivity", "onCreate"+5/0);
+        }
     }
 }
